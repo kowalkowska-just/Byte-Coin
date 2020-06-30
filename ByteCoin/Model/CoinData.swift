44c21cec3e24fca36be83ba61d52f02ebe5bdcec
kowalkowska-just <kowalkowska.just@gmail.com>
2020-07-01 01:33:08 +0200

    The complete app

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Justyna Kowalkowska on 12/05/2020.
+//  Copyright © 2020 Justyna Kowalkowska. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
